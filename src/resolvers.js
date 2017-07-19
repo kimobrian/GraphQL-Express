cf44b6899396236484756bed3930e39af8e7751d
kimobrian <brian.kimokoti@andela.com>
2017-07-20 01:25:37 +0300

    chore(resolvers): Create resolvers

---
+++ src/resolvers.js
@@ -0,0 +1,27 @@
+const channels = [{
+  id: 1,
+  name: 'soccer',
+}, {
+  id: 2,
+  name: 'baseball',
+}];
+
+let nextId = 3;
+
+export const resolvers = {
+  Query: {
+    channels: () => {
+      return channels;
+    },
+    channel: (root, { id }) => {
+      return channels.find(channel => channel.id === id);
+    },
+  },
+  Mutation: {
+    addChannel: (root, args) => {
+      const newChannel = { id: nextId++, name: args.name };
+      channels.push(newChannel);
+      return newChannel;
+    },
+  },
+};
