c4de95b3d71432b50415814a9f0d3500f46a5c1f
kimobrian <brian.kimokoti@andela.com>
2017-09-23 21:23:00 +0300

    feat: Include subscription schema

--- src/schema.js
+++ src/schema.js
@@ -1,33 +1,36 @@
 import {
-  makeExecutableSchema,
+  makeExecutableSchema
 } from 'graphql-tools';
 
 import { resolvers } from './resolvers';
 
 const typeDefs = `
-    type Channel {
-      id: ID!                # "!" denotes a required field
-      name: String
-      messages: [Message]!
-    }
+type Channel {
+  id: ID!                # "!" denotes a required field
+  name: String
+}
 
-    type Message {
-      id: ID!
-      text: String
-    }
-    # This type specifies the entry points into our API. In this case
-    # there is only one - "channels" - which returns a list of channels.
-    type Query {
-      channels: [Channel]    # "[]" means this is a list of channels
-      channel(id: ID!): Channel
-    }
+type Message {
+  id: ID!
+  text: String
+}
 
-    # The mutation root type, used to define all mutations.
-    type Mutation {
-      # A mutation to add a new channel to the list of channels
-      addChannel(name: String!): Channel
-    }
-    `;
+# This type specifies the entry points into our API
+type Query {
+  channels: [Channel]    # "[]" means this is a list of channels
+  channel(id: ID!): Channel
+}
+
+# The mutation root type, used to define all mutations
+type Mutation {
+  addChannel(name: String!): Channel
+}
+
+# The subscription root type, specifying what we can subscribe to
+type Subscription {
+    channelAdded: Channel
+}
+`;
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 export { schema };
