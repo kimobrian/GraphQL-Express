1bf307b4ddeb7183eec55cbdfa9972faf82067fb
O'Brian Kimokoti <brian.kimokoti@andela.com>
2017-10-20 15:11:52 +0300

    Update CORs port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import { schema } from './src/schema';
 
 const PORT = 7700;
 const server = express();
-server.use('*', cors({ origin: 'http://localhost:8000' }));
+server.use('*', cors({ origin: 'http://localhost:7800' }));
 
 server.use('/graphql', bodyParser.json(), graphqlExpress({
   schema
